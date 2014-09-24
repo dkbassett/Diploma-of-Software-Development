b297d930f36009e8e15042a444a7c55dd60e9269
Daniel Bassett <dkbassett@hotmail.com>
2014-09-24 11:13:22 +1000

    Inventory Maintenance
    A simple form that allows the adding or deletion of plant or supply
    items in a price list.

---
+++ Term-3/Daniel_Bassett_OOP2AnotherLang_AS1/OOP2_C#Exercise19/Inheritance/InventoryMaintenance/Plant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Plant : InvItem
+    {
+        // properties
+        public string Size { get; set; }
+
+        // constructors
+        public Plant()
+        {
+        }
+        public Plant(int itemNo, string description, decimal price, string size)
+            : base(itemNo, description, price)
+        {
+            this.Size = size;
+        }
+
+        public override string GetDisplayText()
+        {
+            return base.GetDisplayText().Insert(base.GetDisplayText().LastIndexOf("   ") + 3, this.Size + " ");
+        }
+    }
+}
---
+++ Term-3/Daniel_Bassett_OOP2AnotherLang_AS1/OOP2_C#Exercise19/Inheritance/InventoryMaintenance/Supply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Supply : InvItem
+    {
+        // properties
+        public string Manufacturer { get; set; }
+
+        // constructors
+        public Supply() { }
+
+        public Supply(int itemNo, string description, decimal price, string manufacturer)
+            : base(itemNo, description, price)
+        {
+            this.Manufacturer = manufacturer;
+        }
+
+        public override string GetDisplayText()
+        {
+            return base.GetDisplayText().Insert(base.GetDisplayText().LastIndexOf("   ") + 3, this.Manufacturer + " ");
+        }
+    }
+}
